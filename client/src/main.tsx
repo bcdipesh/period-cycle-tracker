ed19e819d3d62212f650dd126a306ef3b857b845
Dipesh B C <bcdipeshwork@gmail.com>
2025-07-12 16:00:50 -0400

    feat: create 404 page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import './index.css';
-import App from './App.tsx';
+import '@/index.css';
+
+import AppLayout from '@/layouts/app-layout';
+import HomePage from '@/pages/home-page';
+import NotFound from '@/pages/not-found';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
---
+++ client/src/pages/not-found.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router';
+import { ArrowLeft } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+
+export default function NotFound() {
+  return (
+    <section className="flex flex-col justify-center items-center min-h-screen text-center">
+      <header className="space-y-3">
+        <h1 className="bg-gradient-to-r from-rose-600 to-purple-600 bg-clip-text text-6xl font-bold text-transparent dark:from-rose-400 dark:to-purple-400">
+          404
+        </h1>
+        <span className="mb-10 bg-gradient-to-r from-rose-600 to-purple-600 bg-clip-text text-2xl font-bold text-transparent dark:from-rose-400 dark:to-purple-400">
+          Page Not Found
+        </span>
+        <p className="text-muted-foreground mb-8 text-xl leading-relaxed">
+          Oops! The page you're looking for seems to have wandered off. Let's
+          get you back on track with your health journey.
+        </p>
+      </header>
+
+      <Button asChild>
+        <Link to="/">
+          <ArrowLeft /> Go back to home
+        </Link>
+      </Button>
+    </section>
+  );
+}
