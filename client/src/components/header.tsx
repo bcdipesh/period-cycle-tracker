407759479d62767519327b3bdb9f1fac2b91963d
Dipesh B C <bcdipeshwork@gmail.com>
2025-07-12 16:30:50 -0400

    feat: add the ability to toggle between light/dark mode in header component

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,19 +2,21 @@ import { Link } from 'react-router';
 
 import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
+import { ModeToggle } from '@/components/mode-toggle';
 
 export function Header() {
   return (
     <header className="container mx-auto max-w-6xl px-4 py-6">
       <div className="flex items-center justify-between">
         <Logo />
-        <div className="space-x-2">
+        <div className="flex items-center gap-4">
           <Button variant="ghost" asChild>
             <Link to="/sign-in">Sign In</Link>
           </Button>
           <Button asChild>
             <Link to="/sign-up">Sign Up</Link>
           </Button>
+          <ModeToggle />
         </div>
       </div>
     </header>
