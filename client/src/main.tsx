2a6240d83bdc348da9ba9668bdc24b064f61185e
Dipesh B C <bcdipeshwork@gmail.com>
2025-07-12 21:10:33 -0400

    feat: setup clerk for authentication and authorization

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,30 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { ClerkProvider } from '@clerk/clerk-react';
 import '@/index.css';
 
 import AppLayout from '@/layouts/app-layout';
 import HomePage from '@/pages/home-page';
 import NotFound from '@/pages/not-found';
 
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!CLERK_PUBLISHABLE_KEY) {
+  throw new Error('Missing Clerk Publishable Key');
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>
 );
