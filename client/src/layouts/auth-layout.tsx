85a02e55cd5e61adc36ad3e6f7ac034a9a6d09dd
Dipesh B C <bcdipeshwork@gmail.com>
2025-07-13 19:42:04 -0400

    feat: create auth-layout component for sign-in and sign-up routes

---
+++ client/src/layouts/auth-layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router';
+
+import { useTheme } from '@/hooks/use-theme';
+
+export default function AppLayout() {
+  const { theme: currentAppTheme } = useTheme();
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-rose-50 via-white to-purple-50 p-4 antialiased dark:from-rose-950 dark:via-gray-950 dark:to-gray-950">
+      <Outlet context={currentAppTheme} />
+    </div>
+  );
+}
