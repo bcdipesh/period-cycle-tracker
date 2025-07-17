be3833cf9cb73d9edb2a15f0aa5bc78333983d89
Dipesh B C <bcdipeshwork@gmail.com>
2025-07-16 22:11:38 -0400

    feat(dashboard): create `DashboardPage` component
    This commit introduces the `DashboardPage` component, serving as the main landing page for users after successful sign-in or sign-up.

---
+++ client/src/pages/dashboard/dashboard-page.tsx
@@ -0,0 +1,9 @@
+import { CurrentCycle } from './components/current-cycle';
+
+export default function DashboardPage() {
+  return (
+    <main className="mx-auto max-w-md">
+      <CurrentCycle />
+    </main>
+  );
+}
